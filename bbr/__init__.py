9160065d1e206b16e5c2d81491f7ed8b5d272d99
abbe98 <albin.post@gmail.com>
2017-09-03 15:15:12 +0200

    function to turn a local id into a kulturarvsdata compatible

--- bbr/__init__.py
+++ bbr/__init__.py
@@ -72,3 +72,42 @@ def validate_uri(uri):
         return True
     else:
         return False
+
+'''
+NOTES
+
+AFAIK the order of how common each prefix is:
+ - 'raa/bbr'
+ - 'raa/bbrb'
+ - 'raa/bbra'
+ - 'rra/bbrm'
+
+The numbering standard described for the `get_item_type` function does apply both to 'raa/bbr' and the verbose URIs.
+
+TODO
+Validate the order of how common each prefix is.
+Validate that there is no cases where a verbose prefix has a local id not following the sane numbering standard.
+
+'''
+def get_full_uri_from_local_id(local_id):
+    type_bad = 'raa/bbr/' + local_id
+    type_b = 'raa/bbrb/' + local_id
+    type_a = 'raa/bbra/' + local_id
+    type_m + 'raa/bbrm/' + local_id
+
+    if local_id.startswith('214'):
+        if validate_uri(type_b):
+            return type_b
+
+    if local_id.startswith('213'):
+        if validate_uri(type_a):
+            return type_a
+
+    if local_id.startswith('212'):
+        if validate_uri(type_m):
+            return type_m
+
+    if validate_uri(type_bad):
+        return type_bad
+
+    return False
